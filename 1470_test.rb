0e96a12813a68fbf2b19c1cb32a4deb13f0e8496
XueYao <xueyaodeai@gmail.com>
2020-11-18 17:07:11 +0800

    add solution for 1470

---
+++ 1470_test.rb
@@ -0,0 +1,11 @@
+# @param {Integer[]} nums
+# @param {Integer} n
+# @return {Integer[]}
+def shuffle(nums, n)
+  shuffle_arr = []
+  n.times do |inx|
+    shuffle_arr.push(nums[inx], nums[n+inx])
+  end
+  return shuffle_arr
+end
+
