bc699c9b378854a86527204c34483a12ddfd7fed
allancai <allancai@tencent.com>
2021-04-07 15:25:47 +0800

    solve tpc_a

---
+++ tpc_a.rb
@@ -0,0 +1,15 @@
+case_num = $stdin.readline.to_i
+case_num.times do
+  case_i = $stdin.readline.to_i
+  if case_i != 0
+    puts "You are the future of Tencent!"
+  else
+    puts "Good luck and Enjoy TPC!"
+  end
+end
+
+# input
+# 3
+# 1
+# 0
+# 20
