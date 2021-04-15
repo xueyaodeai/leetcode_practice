739570308893b4ebbb4847d4a524dd7708db6502
allancai <allancai@tencent.com>
2021-04-15 16:10:30 +0800

    update tpc_a

--- tpc_a.rb
+++ tpc_a.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 case_num = $stdin.readline.to_i
 case_num.times do
   case_i = $stdin.readline.to_i
   if case_i != 0
-    puts "You are the future of Tencent!"
+    puts 'You are the future of Tencent!'
   else
-    puts "Good luck and Enjoy TPC!"
+    puts 'Good luck and Enjoy TPC!'
   end
 end
 
