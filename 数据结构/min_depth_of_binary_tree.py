6c303aa7837ac61536756c38eea0788c5ab1b3a0
allancai <allancai@tencent.com>
2022-04-02 11:44:58 +0800

    二叉树最小深度增加广度优先方案

--- 数据结构/min_depth_of_binary_tree.py
+++ 数据结构/min_depth_of_binary_tree.py
@@ -24,3 +24,23 @@ def min_depth(root: TreeNode) -> int:
     if root.left is None or root.right is None:
         return d_left + d_right + 1
     return min(d_left, d_right) + 1
+
+
+# 广度优先 BFS
+# 1. 根节点放进数组
+# 2. 根节点为 None 则返回 0
+# 3. 根节点取出，深度 + 1，判断是否为叶子结点(左右子节点都为 None)，是就返回当前深度
+# 4. 非叶子结点就遍历左右子节点，非 None 则放进数组
+# 5. 重复遍历数组直至满足条件
+def min_depth_bfs(root: TreeNode) -> int:
+    queue, depth = [root], 0
+    while root and len(queue) > 0:
+        n, depth = len(queue), depth + 1
+        for _ in range(n):
+            node = queue.pop(0)
+            if not node.left and not node.right:
+                return depth
+            for child in [node.left, node.right]:
+                if child:
+                    queue.append(child)
+    return 0
